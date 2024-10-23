0562ee2f0b3982b64dc4859625a757478ed62820
Rafael Pardinas <rafapi@gmail.com>
2024-10-23 18:23:43 +0100

    Add model names

--- fast_llm/models/auto.py
+++ fast_llm/models/auto.py
@@ -1,4 +1,4 @@
-from fast_llm.models.custom.config import CustomModelConfig, CustomTrainerConfig
+from fast_llm.models.grpo.config import GRPOModelConfig, GRPOTrainerConfig
 from fast_llm.models.gpt.config import GPTModelConfig, GPTTrainerConfig
 from fast_llm.utils import Registry
 
@@ -6,7 +6,7 @@ model_registry = Registry(
     "Model",
     {
         "gpt": GPTModelConfig,
-        "gpt_custom": CustomModelConfig,
+        "grpo": GRPOModelConfig,
     },
 )
 
@@ -14,6 +14,6 @@ trainer_registry = Registry(
     "Model",
     {
         "gpt": GPTTrainerConfig,
-        "gpt_custom": CustomTrainerConfig,
+        "grpo": GRPOTrainerConfig,
     },
 )
