55b0b88ea466946bb4f13d8c8823cc442fd9cf81
Torsten Scholak <torsten.scholak@googlemail.com>
2024-11-11 18:29:17 -0500

    use full imports

--- tests/test_memmap_dataset.py
+++ tests/test_memmap_dataset.py
@@ -1,11 +1,13 @@
-import hypothesis
-import hypothesis.strategies
-import hypothesis.extra.numpy
-import numpy as np
+import pathlib
 from tempfile import TemporaryDirectory
-from pathlib import Path
-from fast_llm.data.gpt.memmap import GPTMemmapDataset
+
+import hypothesis
+import hypothesis.extra.numpy
+import hypothesis.strategies
+import numpy as np
 import pytest
+
+from fast_llm.data.gpt.memmap import GPTMemmapDataset
 
 
 def dtype_arrays(dtype: np.dtype, min_size: int = 1, max_size: int = 100) -> hypothesis.strategies.SearchStrategy:
@@ -21,7 +23,7 @@ def test_gpt_memmap_dataset(dtype):
     @hypothesis.given(documents=dtype_arrays(dtype))
     def inner_test(documents):
         with TemporaryDirectory() as temp_dir:
-            prefix = Path(temp_dir)
+            prefix = pathlib.Path(temp_dir)
             GPTMemmapDataset.write_dataset(prefix=prefix, documents=documents)
             dataset = GPTMemmapDataset(name="foo", prefix=prefix)
             for i, document in enumerate(documents):
