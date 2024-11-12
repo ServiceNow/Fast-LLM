4827f492ff668830afe942fa2e48f677876ece23
Joel Lamy-Poirier <joel.lamy-poirier@servicenow.com>
2024-11-12 18:31:32 -0500

    Faster tests

--- tests/test_memmap_dataset.py
+++ tests/test_memmap_dataset.py
@@ -1,9 +1,6 @@
 import pathlib
 import tempfile
 
-import hypothesis
-import hypothesis.extra.numpy
-import hypothesis.strategies
 import numpy as np
 import pytest
 
@@ -11,23 +8,14 @@ from fast_llm.data.gpt.memmap import GPTMemmapDataset
 from fast_llm.data.preparator.gpt_memmap.config import MEMMAP_DTYPES
 
 
-def dtype_arrays(dtype: np.dtype, min_size: int = 1, max_size: int = 100) -> hypothesis.strategies.SearchStrategy:
-    return hypothesis.strategies.lists(
-        hypothesis.extra.numpy.arrays(dtype=dtype, shape=hypothesis.strategies.integers(1, 1000)),
-        min_size=min_size,
-        max_size=max_size,
-    )
-
-
 @pytest.mark.parametrize("dtype", MEMMAP_DTYPES.values())
 def test_gpt_memmap_dataset(dtype):
-    @hypothesis.given(documents=dtype_arrays(dtype))
-    def inner_test(documents):
-        with tempfile.TemporaryDirectory() as temp_dir:
-            prefix = pathlib.Path(temp_dir)
-            GPTMemmapDataset.write_dataset(prefix=prefix, documents=documents)
-            dataset = GPTMemmapDataset(name="foo", prefix=prefix)
-            for i, document in enumerate(documents):
-                assert np.array_equal(dataset.get(i), document, equal_nan=True), f"Mismatch at index {i}"
-
-    inner_test()
+    documents = [np.random.randint(1000, size=np.random.randint(1, 100)) for _ in range(100)]
+    with tempfile.TemporaryDirectory() as temp_dir:
+        prefix = pathlib.Path(temp_dir)
+        GPTMemmapDataset.write_dataset(prefix=prefix, documents=documents)
+        dataset = GPTMemmapDataset(name="foo", prefix=prefix)
+        for i, document in enumerate(documents):
+            assert np.array_equal(
+                dataset.get(i), document, equal_nan=True
+            ), f"Mismatch for document {i}: {document} != {dataset.get(i)}."
