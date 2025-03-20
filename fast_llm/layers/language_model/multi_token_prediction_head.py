e6b5e27b42a9f666529ea5c63335982eea68a6e4
Toolkit User <raymond.li@servicenow.com>
2025-03-20 20:35:26 +0000

    remove comments

--- fast_llm/layers/language_model/multi_token_prediction_head.py
+++ fast_llm/layers/language_model/multi_token_prediction_head.py
@@ -14,10 +14,6 @@ from fast_llm.layers.transformer.transformer import TransformerLayer
 from fast_llm.tensor import TensorMeta
 
 
-def grad_is_context_sum(grad_output: torch.Tensor, context: torch.Tensor) -> torch.Tensor:  # noqa
-    return grad_output + context
-
-
 class MultiTokenPredictionTransformerLayer(TransformerLayer):
     """
     A transformer layer for multi-token prediction.
@@ -94,7 +90,6 @@ class MultiTokenPredictionLanguageModelHead(LanguageModelHead):
         else:
             # Backward hook to compute the gradient of the loss
             shared_hidden = AuxiliaryLoss.apply(shared_hidden, language_model_loss, 1.0)
-            # TODO: Return the model output when needed.
             # MTP: Return shared_hidden to be used by the next head.
             return shared_hidden
 
@@ -106,8 +101,6 @@ class MultiTokenPredictionLanguageModelHead(LanguageModelHead):
         labels = labels[:, self.multi_token_prediction_index :].flatten() if labels is not None else None
         if self._sequence_parallel_logits:
             raise NotImplementedError()
-            # TODO MTP
-            labels = split_op(labels, self._tensor_space.distributed.tensor_group, 0)
         do_grad = labels is not None and self.training
         input_ = input_.detach().requires_grad_(do_grad)
         with torch.enable_grad():
@@ -137,19 +130,3 @@ class MultiTokenPredictionLanguageModelHead(LanguageModelHead):
         if self.multi_token_prediction_index > 0:
             return kwargs[WORD_EMBEDDINGS_WEIGHT] if self._tie_word_embeddings else kwargs[OUTPUT_WEIGHTS]
         return super()._get_output_weights(kwargs)
-
-    # def _logits_cross_entropy_forward_backward_split(
-    #     self,
-    #     input_: torch.Tensor,
-    #     labels: torch.Tensor | None,
-    #     weight: torch.Tensor,
-    #     grad_output: float,
-    #     kwargs: dict,
-    #     losses: dict | None = None,
-    # ) -> tuple[torch.Tensor | None, torch.Tensor | None]:
-    #     if self.multi_token_prediction_index > 0:
-    #         # This MTP-head is not the first one, so the labels need to be shifted.
-    #         if labels is not None:
-    #             labels = labels[self.multi_token_prediction_index:]
-    #         input_ = input_[:-self.multi_token_prediction_index]
-    #     return super()._logits_cross_entropy_forward_backward_split(input_, labels, weight, grad_output, kwargs, losses)
