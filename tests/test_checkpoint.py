c6356c2bbeffb8ee9a162a9df9d68c8dfaec592e
Toolkit User <denis.kocetov@servicenow.com>
2025-03-27 13:56:39 +0000

    changed test configs to the new naming conventions

--- tests/test_checkpoint.py
+++ tests/test_checkpoint.py
@@ -46,8 +46,8 @@ def test_checkpoint_and_eval():
         CONFIG_COMMON
         + [
             "training.checkpoint.interval=1",
-            "training.validation.interval=2",
-            "training.validation.iterations=1",
+            "training.evaluations.validation.interval=2",
+            "training.evaluations.validation.iterations=1",
         ],
     )
 
@@ -79,8 +79,8 @@ def test_resume():
         CONFIG_COMMON
         + [
             "training.checkpoint.interval=1",
-            "training.validation.interval=2",
-            "training.validation.iterations=1",
+            "training.evaluations.validation.interval=2",
+            "training.evaluations.validation.iterations=1",
         ],
         compare=f"test_{TEST_MODEL}_checkpoint_and_eval",
         prepare_fn=_prepare_resume_fn,
