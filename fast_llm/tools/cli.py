df032b59794dd84384840294074200439b84dc24
oleksost <ostapy2@gmail.com>
2025-04-23 15:53:47 +0000

    nvm

--- fast_llm/tools/cli.py
+++ fast_llm/tools/cli.py
@@ -5,6 +5,7 @@ import traceback
 
 from fast_llm.config import ValidationError
 from fast_llm.engine.config_utils.logging import configure_logging
+from fast_llm.engine.config_utils.run import log_main_rank
 
 logger = logging.getLogger(__name__)
 
@@ -29,7 +30,7 @@ def fast_llm(args=None):
     except ValidationError:
         if sys.gettrace():
             raise
-        logger.critical(traceback.format_exc())
+        log_main_rank(traceback.format_exc(), log_fn=logger.error)
         sys.exit(1)
     except Exception:  # noqa
         if sys.gettrace():
