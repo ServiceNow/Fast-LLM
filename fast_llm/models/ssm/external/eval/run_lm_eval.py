a50bc2e1e414b89718f72086056bdbd1cbc01106
oleksost <ostapy2@gmail.com>
2025-05-01 20:38:37 +0000

    cleanup

--- fast_llm/models/ssm/external/eval/run_lm_eval.py
+++
@@ -1,6 +0,0 @@
-from lm_eval.__main__ import cli_evaluate
-
-from fast_llm.models.ssm.external.eval.apriel_eval_wrapper import AprielSSMWrapper  # noqa: F401
-
-if __name__ == "__main__":
-    cli_evaluate()
