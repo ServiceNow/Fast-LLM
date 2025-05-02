a68c0b7318ec07abebea8acc0c035f9f8877017e
oleksost <ostapy2@gmail.com>
2025-05-02 14:30:10 +0000

    conversion

--- fast_llm/models/ssm/conversion.py
+++ fast_llm/models/ssm/conversion.py
@@ -52,11 +52,9 @@ class HybridModelCheckpointHandler(HuggingfaceStateDictCheckpointHandler):
     @classmethod
     def _create_config_converters(cls) -> list[ParamConverter]:
         if cls.block_pattern is not None:
-            block_converter = (
-                RenameParamConverter(
-                    fast_llm_names=(("hybrid_block_layout",),),
-                    export_names=(("hybrid_block_layout",),),
-                ),
+            block_converter = RenameParamConverter(
+                fast_llm_names=(("hybrid_block_layout",),),
+                export_names=(("hybrid_block_layout",),),
             )
         else:
             block_converter = ConstantImportParamConverter(
