a250554e117872188c68ee45b8146c2ae2deb19a
Luke Kumar <luke.kumar@servicenow.com>
2025-06-01 01:49:50 +0000

    fix

--- fast_llm/models/ssm/external/apriel_15b_hybrid/configuration_ssm_hybrid_apriel15b.py
+++ fast_llm/models/ssm/external/apriel_15b_hybrid/configuration_ssm_hybrid_apriel15b.py
@@ -10,14 +10,15 @@ class AprielSSMHybridConfig(MistralConfig):
     def __init__(self, hybrid_block_layout=["m2d"], ssm_cfg=None, **kwargs):
         super().__init__(**kwargs)
         self.hybrid_block_layout = hybrid_block_layout
-        self.ssm_cfg = ssm_cfg or {
-            "d_state": 64,
-            "n_v_heads": 24,
-            "n_qk_heads": 24,
-            "expand": 1,
-            "chunk_size": 128,
-            "activation": "identity",
-            "bias": False,
-            "d_conv": 4,
-            "d_inner": 24 * self.head_dim,  # num_heads * head_dim
-        }
+        self.ssm_cfg = ssm_cfg
+        # or {
+        #     "d_state": 64,
+        #     "n_v_heads": 24,
+        #     "n_qk_heads": 24,
+        #     "expand": 1,
+        #     "chunk_size": 128,
+        #     "activation": "identity",
+        #     "bias": False,
+        #     "d_conv": 4,
+        #     "d_inner": 24 * self.head_dim,  # num_heads * head_dim
+        # }
