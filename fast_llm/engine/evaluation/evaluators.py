77603a285dca62203b268ae906775cdbd5109a84
bigximik <denisko@live.com>
2025-06-18 06:56:49 +0000

    more concise evaluate implementation suggested by Joel

--- fast_llm/engine/evaluation/evaluators.py
+++ fast_llm/engine/evaluation/evaluators.py
@@ -1,22 +1,12 @@
-import typing
-
 from fast_llm.config import config_class
 from fast_llm.engine.config_utils.runnable import RunnableConfig
+from fast_llm.engine.training.config import TrainerConfig
 
 
-@config_class(registry=True, dynamic_type={RunnableConfig: "evaluate"})
+@config_class(dynamic_type={RunnableConfig: "evaluate"})
 class EvaluatorsConfig(RunnableConfig):
-    _abstract = True
 
     @classmethod
-    def _from_dict(
-        cls,
-        default: dict[str, typing.Any],
-        strict: bool = True,
-        flat: bool = False,
-    ) -> typing.Self:
-        if "training" in default:
-            default["training"]["train_iters"] = 0
-        else:
-            default["training"] = {"train_iters": 0}
-        return super()._from_dict(default, strict, flat)
+    def parse_and_run(cls, args: list[str] | None = None) -> None:
+        args.append("training.train_iters=0")
+        return TrainerConfig.parse_and_run(args)
--- fast_llm/models/auto.py
+++ fast_llm/models/auto.py
@@ -6,6 +6,4 @@ from fast_llm.models.custom.config import CustomModelConfig, CustomTrainerConfig
 from fast_llm.models.gpt.config import GPTModelConfig, GPTTrainerConfig  # isort: skip
 from fast_llm.models.ssm.config import HybridSSMModelConfig, HybridSSMTrainerConfig  # isort: skip
 
-from fast_llm.models.custom.evaluators import CustomEvaluatorsConfig  # isort: skip
-from fast_llm.models.gpt.evaluators import GPTEvaluatorsConfig  # isort: skip
-from fast_llm.models.ssm.evaluators import HybridSSMEvaluatorsConfig  # isort: skip
+from fast_llm.engine.evaluation.evaluators import EvaluatorsConfig  # isort: skip
