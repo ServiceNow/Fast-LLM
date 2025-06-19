41198540fcc611ee147cd535a04435b5a86a68b2
Denis Kochetkov <denis.kocetkov@servicenow.com>
2025-06-19 18:44:14 +0300

    Refactoring of Evaluation and adding of evaluate command (#264)

--- fast_llm/data/tokenizer.py
+++ fast_llm/data/tokenizer.py
@@ -1,6 +1,6 @@
 import numpy as np
 import torch
-from transformers import PreTrainedTokenizerFast
+from transformers import AutoTokenizer
 
 from fast_llm.data.config import TokenizerConfig
 from fast_llm.engine.config_utils.run import log_main_rank
@@ -13,9 +13,15 @@ class Tokenizer:
 
     def __init__(self, config: TokenizerConfig):
         log_main_rank(f"> loading tokenizer from {config.path} ...")
-        self.tokenizer: PreTrainedTokenizerFast = PreTrainedTokenizerFast.from_pretrained(
-            pretrained_model_name_or_path=config.path, errors="replace", max_len=None
+        self.tokenizer = AutoTokenizer.from_pretrained(
+            pretrained_model_name_or_path=config.path,
+            errors="replace",
+            max_len=None,
+            trust_remote_code=True,
+            use_fast=True,
         )
+        if config.bos_token is not None:
+            self.tokenizer.bos_token = config.bos_token
         if self.tokenizer.eos_token_id is None:
             raise ValueError("Tokenizer does not have an EOS token.")
         if self.tokenizer.bos_token_id is None:
@@ -52,7 +58,7 @@ class Tokenizer:
         token_spans = []
         char_pos = 0
         beginning_of_text = True
-        
+
         for start, end in char_spans:
             if char_pos < start:
                 curr_text = text[char_pos:start]
--- fast_llm/models/auto.py
+++ fast_llm/models/auto.py
@@ -5,3 +5,5 @@ Import these submodules to ensure classes are added to the dynamic class registr
 from fast_llm.models.custom.config import CustomModelConfig, CustomTrainerConfig  # isort: skip
 from fast_llm.models.gpt.config import GPTModelConfig, GPTTrainerConfig  # isort: skip
 from fast_llm.models.ssm.config import HybridSSMModelConfig, HybridSSMTrainerConfig  # isort: skip
+
+from fast_llm.engine.evaluation.evaluators import EvaluatorsConfig  # isort: skip
