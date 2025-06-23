dcaf28b743c49cef0a3feacee1df0788982769ed
Nanda H Krishna <me@nandahkrishna.com>
2025-06-23 22:14:26 +0000

    Update config MTP

--- fast_llm/models/ssm/external/apriel_15b_hybrid/configuration_ssm_hybrid_apriel15b.py
+++ fast_llm/models/ssm/external/apriel_15b_hybrid/configuration_ssm_hybrid_apriel15b.py
@@ -3,6 +3,18 @@ from transformers.utils import logging
 
 logger = logging.get_logger(__name__)
 
+ssm_config_default = {
+    "d_state": 64,
+    "n_v_heads": 32,
+    "n_qk_heads": 32,
+    "expand": 1,
+    "chunk_size": 128,
+    "activation": "identity",
+    "bias": False,
+    "d_conv": 4,
+    "d_inner": 32 * 128,
+}
+
 
 class AprielSSMHybridConfig(MistralConfig):
     model_type = "apriel_ssm_thinker_hybrid"
@@ -11,18 +23,14 @@ class AprielSSMHybridConfig(MistralConfig):
         super().__init__(**kwargs)
         self.hybrid_block_layout = hybrid_block_layout
         self.set_if_mtp_model_type(prediction_heads)
+        self.head_dim = self.head_dim or self.hidden_size // self.num_attention_heads
         self.prediction_heads = prediction_heads
-        self.ssm_cfg = ssm_cfg or {
-            "d_state": 64,
-            "n_v_heads": 24,
-            "n_qk_heads": 24,
-            "expand": 1,
-            "chunk_size": 128,
-            "activation": "identity",
-            "bias": False,
-            "d_conv": 4,
-            "d_inner": 24 * self.head_dim,  # num_heads * head_dim
-        }
+        self.ssm_cfg = ssm_cfg or ssm_config_default
+
+        for key, value in ssm_config_default.items():
+            if key not in self.ssm_cfg:
+                logger.warning(f"SSM config key '{key}' not found in provided ssm_cfg. Using default value: {value}")
+                self.ssm_cfg[key] = value
 
     @classmethod
     def set_if_mtp_model_type(cls, prediction_heads):
