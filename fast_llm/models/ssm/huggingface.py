a7e5b460b71fc1f89be2d7969938a2ace44f6532
bigximik <denisko@live.com>
2025-07-16 09:37:42 +0000

    support for ssm HF wrapper

--- fast_llm/models/ssm/huggingface.py
+++ fast_llm/models/ssm/huggingface.py
@@ -1,9 +1,10 @@
 import logging
+import typing
 
-from fast_llm.engine.huggingface.config import HuggingfaceModelConfig
+from fast_llm.engine.inference.config import HuggingfaceModelConfig
 from fast_llm.models.gpt.huggingface import HuggingfaceGPTModelForCausalLM
 from fast_llm.models.ssm.config import HybridSSMModelConfig
-from fast_llm.models.ssm.model import HybridSSMModel
+from fast_llm.models.ssm.model import HybridSSMInferenceRunner, HybridSSMModel
 
 logger = logging.getLogger(__name__)
 
@@ -18,4 +19,5 @@ class HuggingfaceHybridSSMModelForCausalLM(HuggingfaceGPTModelForCausalLM):
     config_class = HuggingfaceSSMModelConfig
     config: HuggingfaceSSMModelConfig
     model_class = HybridSSMModel
+    runner_class: typing.ClassVar[type[HybridSSMInferenceRunner]] = HybridSSMInferenceRunner
     _fast_llm_model: HybridSSMModel
