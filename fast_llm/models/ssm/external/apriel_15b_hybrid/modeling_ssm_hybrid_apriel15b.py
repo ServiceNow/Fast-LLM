be5da95f31d69c17f5e10ce184727066eb8e8658
oleksost <ostapy2@gmail.com>
2025-07-16 00:31:51 +0000

    clean modeling

--- fast_llm/models/ssm/external/apriel_15b_hybrid/modeling_ssm_hybrid_apriel15b.py
+++ fast_llm/models/ssm/external/apriel_15b_hybrid/modeling_ssm_hybrid_apriel15b.py
@@ -528,13 +528,13 @@ class DiscreteMamba2(nn.Module):
         # self.zeros_buffer = torch.zeros((self.n_v_heads, self.headdim), device=device, dtype=dtype)
         # self.ones_buffer = torch.ones((self.n_v_heads, self.headdim, self.d_state), device=device, dtype=dtype)
 
-    @property
-    def d_output(self):
-        return self.d_model
+    # @property
+    # def d_output(self):
+    #     return self.d_model
 
-    @property
-    def state_to_tensor(self):
-        return self.layer.state_to_tensor
+    # @property
+    # def state_to_tensor(self):
+    #     return self.layer.state_to_tensor
 
     def forward(
         self,
@@ -552,7 +552,7 @@ class DiscreteMamba2(nn.Module):
         assert is_fast_path_available and "cuda" in self.in_proj.weight.device.type, "Only support fast path on cuda"
         cache_position = kwargs.get("cache_position", None)
         batch, seqlen, dim = u.shape
-        # u = apply_mask_to_padding_states(u, attention_mask)
+        u = apply_mask_to_padding_states(u, attention_mask)
         ssm_state, conv_state = None, None
         use_precomputed_states = False
         #########################################################
@@ -1208,115 +1208,6 @@ class AprielThinkerSSMHybridModel(MistralModel):
 
         # Initialize weights and apply final processing
         self.post_init()
-
-    # @can_return_tuple
-    # @auto_docstring
-    # def forward(
-    #     self,
-    #     input_ids: Optional[torch.LongTensor] = None,
-    #     attention_mask: Optional[torch.Tensor] = None,
-    #     position_ids: Optional[torch.LongTensor] = None,
-    #     past_key_values: Optional[HybridMambaAttentionDynamicCache] = None,
-    #     inputs_embeds: Optional[torch.FloatTensor] = None,
-    #     use_cache: Optional[bool] = None,
-    #     output_attentions: Optional[bool] = None,
-    #     output_hidden_states: Optional[bool] = None,
-    #     cache_position: Optional[torch.LongTensor] = None,
-    #     **flash_attn_kwargs: Unpack[FlashAttentionKwargs],
-    # ) -> BaseModelOutputWithPast:
-    #     output_attentions = output_attentions if output_attentions is not None else self.config.output_attentions
-    #     output_hidden_states = (
-    #         output_hidden_states if output_hidden_states is not None else self.config.output_hidden_states
-    #     )
-    #     use_cache = use_cache if use_cache is not None else self.config.use_cache
-
-    #     if (input_ids is None) ^ (inputs_embeds is not None):
-    #         raise ValueError("You must specify exactly one of input_ids or inputs_embeds")
-
-    #     if self.gradient_checkpointing and self.training and use_cache:
-    #         logger.warning_once(
-    #             "`use_cache=True` is incompatible with gradient checkpointing. Setting `use_cache=False`."
-    #         )
-    #         use_cache = False
-
-    #     if inputs_embeds is None:
-    #         inputs_embeds = self.embed_tokens(input_ids)
-
-    #     # OO: Cache is initialized in the `prepare_inputs_for_generation` method, so this can be removed
-    #     # if use_cache and past_key_values is None:
-    #     #     past_key_values = DynamicCache()
-
-    #     if cache_position is None:
-    #         past_seen_tokens = past_key_values.get_seq_length() if past_key_values is not None else 0
-    #         cache_position = torch.arange(
-    #             past_seen_tokens, past_seen_tokens + inputs_embeds.shape[1], device=inputs_embeds.device
-    #         )
-
-    #     if position_ids is None:
-    #         position_ids = cache_position.unsqueeze(0)
-
-    #     causal_mask = self._update_causal_mask(
-    #         attention_mask, inputs_embeds, cache_position, past_key_values, output_attentions
-    #     )
-
-    #     hidden_states = inputs_embeds
-
-    #     # create position embeddings to be shared across the decoder layers
-    #     position_embeddings = self.rotary_emb(hidden_states, position_ids)
-
-    #     # decoder layers
-    #     all_hidden_states = () if output_hidden_states else None
-    #     all_self_attns = () if output_attentions else None
-
-    #     for decoder_layer in self.layers[: self.config.num_hidden_layers]:
-    #         if output_hidden_states:
-    #             all_hidden_states += (hidden_states,)
-
-    #         if self.gradient_checkpointing and self.training:
-    #             layer_outputs = self._gradient_checkpointing_func(
-    #                 partial(decoder_layer.__call__, **flash_attn_kwargs),
-    #                 hidden_states,
-    #                 causal_mask,
-    #                 position_ids,
-    #                 past_key_values,
-    #                 output_attentions,
-    #                 use_cache,
-    #                 cache_position,
-    #                 position_embeddings,
-    #             )
-    #         else:
-    #             layer_outputs = decoder_layer(
-    #                 hidden_states,
-    #                 attention_mask=causal_mask,
-    #                 position_ids=position_ids,
-    #                 past_key_value=past_key_values,
-    #                 output_attentions=output_attentions,
-    #                 use_cache=use_cache,
-    #                 cache_position=cache_position,
-    #                 position_embeddings=position_embeddings,
-    #                 **flash_attn_kwargs,
-    #             )
-
-    #         hidden_states = layer_outputs[0]
-
-    #         if output_attentions:
-    #             all_self_attns += (layer_outputs[1],)
-
-    #     hidden_states = self.norm(hidden_states)
-
-    #     # add hidden states from the last decoder layer
-    #     if output_hidden_states:
-    #         all_hidden_states += (hidden_states,)
-
-    #     if past_key_values and not past_key_values.has_previous_state:
-    #         past_key_values.has_previous_state = True
-
-    #     return BaseModelOutputWithPast(
-    #         last_hidden_state=hidden_states,
-    #         past_key_values=past_key_values if use_cache else None,
-    #         hidden_states=all_hidden_states,
-    #         attentions=all_self_attns,
-    #     )
 
 
 class KwargsForCausalLM(FlashAttentionKwargs, LossKwargs): ...
