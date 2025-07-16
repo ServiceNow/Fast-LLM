807df7c26f2d6cd339d90066d790229e6543a799
oleksost <ostapy2@gmail.com>
2025-07-16 18:18:04 +0000

    external

--- fast_llm/models/ssm/external/apriel_15b_hybrid/configuration_ssm_hybrid_apriel15b.py
+++ fast_llm/models/ssm/external/apriel_15b_hybrid/configuration_ssm_hybrid_apriel15b.py
@@ -2,9 +2,9 @@ from transformers import MistralConfig
 from transformers.utils import logging
 
 logger = logging.get_logger(__name__)
-# TODO: split into mamba 2 and discrete mamba 2 configs ith a base dict
+# TODO: split into mamba 2 and discrete mamba 2 configs with a base dict
 ssm_config_default = {
-    # descrete mamba2
+    # discrete mamba2
     "d_state": 64,
     "n_v_heads": 32,
     "n_qk_heads": 32,
@@ -15,7 +15,7 @@ ssm_config_default = {
     "d_conv": 4,
     "d_inner": 32 * 128,
     # mamba2
-    "d_xb": None,  # willb e set to model dim
+    "d_xb": None,  # will be set to model dim
     "dt_rank": "auto",
     "dt_min": 0.001,
     "dt_max": 0.1,
