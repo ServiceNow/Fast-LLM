d1caa987e648719ca8418c8bd93a2f31a46e7d2c
Toolkit User <raymond.li@servicenow.com>
2025-07-17 20:47:42 +0000

    fix name

--- fast_llm/layers/ssm/config.py
+++ fast_llm/layers/ssm/config.py
@@ -22,7 +22,7 @@ class SSMDimNames:
     v_heads = "v_heads"  # Number of V heads
 
     # Mamba 2
-    x_proj_dim_2 = "x_proj_dim"  # d_xb
+    x_proj_dim_2 = "x_proj_dim_2"  # d_xb
 
 
 class SSMBlockType(enum.StrEnum):
