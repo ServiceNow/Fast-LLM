30928800b56c0836f91baaf9627de2d78937a34d
Toolkit User <raymond.li@servicenow.com>
2025-07-18 19:20:57 +0000

    fix model name

--- fast_llm/models/ssm/external/make_hybrid_checkpoint.py
+++ fast_llm/models/ssm/external/make_hybrid_checkpoint.py
@@ -5,7 +5,9 @@ import torch
 from transformers import AutoConfig, AutoModelForCausalLM
 
 from fast_llm.models.ssm.external.apriel_15b_hybrid.configuration_ssm_hybrid_apriel15b import AprielSSMHybridConfig
-from fast_llm.models.ssm.external.apriel_15b_hybrid.modeling_ssm_hybrid_apriel15b import AprielSSMHybridForCausalLM
+from fast_llm.models.ssm.external.apriel_15b_hybrid.modeling_ssm_hybrid_apriel15b import (
+    AprielThinkerSSMHybridForCausalLM,
+)
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
@@ -22,7 +24,7 @@ def main(checkpoint: str, identity_index: int, save_dir: str):
         hybrid_block_layout[identity_index] = "i"
 
     hybrdif_apriel_config = AprielSSMHybridConfig(**config.to_dict(), hybrid_block_layout=hybrid_block_layout)
-    hybrid_apriel_model = AprielSSMHybridForCausalLM(hybrdif_apriel_config)
+    hybrid_apriel_model = AprielThinkerSSMHybridForCausalLM(hybrdif_apriel_config)
     hybrid_apriel_model.to(dtype=torch.bfloat16).to(device)
 
     apriel_model = AutoModelForCausalLM.from_pretrained(checkpoint, torch_dtype=torch.bfloat16, trust_remote_code=True)
