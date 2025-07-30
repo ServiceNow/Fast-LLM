c850c27034d4a96cdfe3f2cf3892ad227c3020e8
oleksost <ostapy2@gmail.com>
2025-07-30 15:14:42 +0000

    modeling

--- fast_llm/models/ssm/external/apriel_15b_hybrid/modeling_ssm_hybrid_apriel15b.py
+++ fast_llm/models/ssm/external/apriel_15b_hybrid/modeling_ssm_hybrid_apriel15b.py
@@ -1049,7 +1049,7 @@ class Mamba2(nn.Module):
         A = -torch.exp(self.A_log.float())  # (d_inner, d_state)
 
         zxbc = self.in_proj(hidden_states_input)
-        z, x, B, C = torch.split(zxbc, [self.d_inner, self.d_xb, self.d_xb, self.d_inner, self.dt_rank], dim=-1)
+        z, x, B, C = torch.split(zxbc, [self.d_inner, self.d_xb, self.d_xb, self.d_inner], dim=-1)
 
         B = rearrange(B, "b (n_group dstate) -> b n_group dstate", dstate=self.d_state)
         B = torch.repeat_interleave(B, dim=1, repeats=self.repeat_group)
