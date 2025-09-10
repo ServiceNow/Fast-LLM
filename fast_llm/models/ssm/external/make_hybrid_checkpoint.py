29125fbf8435da992cce609460b7ffea140eb497
Toolkit User <raymond.li@servicenow.com>
2025-09-10 08:44:03 +0000

    update convert_layers

--- fast_llm/models/ssm/external/make_hybrid_checkpoint.py
+++ fast_llm/models/ssm/external/make_hybrid_checkpoint.py
@@ -13,8 +13,15 @@ from fast_llm.models.ssm.external.apriel_15b_hybrid.modeling_ssm_hybrid_apriel15
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
 
-def convert_layers(transformer, mamba_config, hybrid_block_layout, init_with_kqvo, torch_dtype=torch.bfloat16):
-    config = transformer.config
+def convert_layers(
+    transformer_config,
+    transformer_model,
+    mamba_config,
+    hybrid_block_layout,
+    init_with_kqvo,
+    torch_dtype=torch.bfloat16,
+):
+    config = transformer_config
     embed_dim = config.hidden_size
     num_heads = config.num_attention_heads
     num_heads_kv = config.num_key_value_heads
@@ -25,7 +32,7 @@ def convert_layers(transformer, mamba_config, hybrid_block_layout, init_with_kqv
     for layer_idx, type in enumerate(hybrid_block_layout):
         print("Converting layer %d...", layer_idx)
         # Fetch the layer module for easier access
-        layer_module = transformer.model.layers._modules[f"{layer_idx}"]
+        layer_module = transformer_model.layers._modules[f"{layer_idx}"]
         if type == "t":
             print("Skipping transformer layer %d..." % layer_idx)
         elif type == "m2":
@@ -63,20 +70,22 @@ def convert_layers(transformer, mamba_config, hybrid_block_layout, init_with_kqv
 
                 print("Init Mamba using Attention")
 
-            transformer.model.layers[layer_idx] = mamba_encoder
+            transformer_model.layers[layer_idx] = mamba_encoder
 
         else:
             raise ValueError(f"Invalid layer type: {type}")
 
 
 @click.command()
+@click.option(
+    "--base_checkpoint", type=str, required=False, default="/mnt/checkpoints/upstream/Apriel-Nemotron-15b-Thinker"
+)
 @click.option("--m2_indexes", type=int, multiple=True, required=True)
 @click.option("--hybrid_checkpoint", type=str, required=True)
 @click.option("--save_dir", type=str, required=True)
-def main(m2_indexes: list, hybrid_checkpoint: str, save_dir: str):
+def main(base_checkpoint: str, m2_indexes: list, hybrid_checkpoint: str, save_dir: str):
     m2_indexes = list(m2_indexes)  # convert tuple -> list
-    path_base = "/mnt/checkpoints/upstream/Apriel-Nemotron-15b-Thinker"
-    transformer = AutoModelForCausalLM.from_pretrained(path_base, trust_remote_code=True)
+    transformer = AutoModelForCausalLM.from_pretrained(base_checkpoint, trust_remote_code=True)
     hybrid_config = AprielSSMHybridConfig.from_pretrained(hybrid_checkpoint)
 
     hybrid_block_layout = hybrid_config.hybrid_block_layout
@@ -84,7 +93,7 @@ def main(m2_indexes: list, hybrid_checkpoint: str, save_dir: str):
         hybrid_block_layout[m2_index] = "m2"
     print(hybrid_block_layout)
 
-    convert_layers(transformer, hybrid_config, hybrid_block_layout, True, torch.bfloat16)
+    convert_layers(transformer.config, transformer.model, hybrid_config, hybrid_block_layout, True, torch.bfloat16)
     hybrid_config.ssm_cfg["activation"] = "silu"
 
     # load all existing  ssm layers
