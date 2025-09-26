ddf21437065da569884b6078a2d4ad8d0b35735c
Joel Lamy-Poirier <joel.lamy-poirier@servicenow.com>
2025-09-26 15:50:26 -0400

    cleanup

--- fast_llm/functional/config.py
+++ fast_llm/functional/config.py
@@ -40,7 +40,6 @@ class ActivationType(enum.StrEnum):
     """
 
     gelu = "gelu"
-    gelu_pytorch_tanh = "gelu_pytorch_tanh"
     silu = "silu"
     relu = "relu"
     squared_relu = "squared_relu"
@@ -68,8 +67,7 @@ def _set_activation_fn_map() -> None:
     global _ACTIVATION_FN_MAP
 
     _ACTIVATION_FN_MAP = {
-        ActivationType.gelu: torch.nn.functional.gelu,
-        ActivationType.gelu_pytorch_tanh: lambda x: torch.nn.functional.gelu(x, approximate="tanh"),
+        ActivationType.gelu: lambda x: torch.nn.functional.gelu(x, approximate="tanh"),
         ActivationType.silu: torch.nn.functional.silu,
         ActivationType.relu: torch.nn.functional.relu,
         ActivationType.squared_relu: lambda x: torch.pow(torch.nn.functional.relu(x), 2),
@@ -80,8 +78,7 @@ def _set_activation_fn_map() -> None:
 _ACTIVATION_FN_MAP: dict[ActivationType, typing.Callable[["torch.Tensor"], "torch.Tensor"]] = {}
 
 _ACTIVATION_HF_NAMES = {
-    ActivationType.gelu: "gelu",
-    ActivationType.gelu_pytorch_tanh: "gelu_pytorch_tanh",
+    ActivationType.gelu: "gelu_pytorch_tanh",
     ActivationType.silu: "silu",
     ActivationType.relu: "relu",
     ActivationType.squared_relu: "relu2",
@@ -89,14 +86,7 @@ _ACTIVATION_HF_NAMES = {
 }
 _ACTIVATION_HF_NAMES_INV = {value: key for key, value in _ACTIVATION_HF_NAMES.items()}
 
-
 MAX_DROPLESS_BLOCK_SIZE_ROW = 128
-
-
-class ReverseKLImpl(str, enum.Enum):
-    tp = "tp"
-    stp = "stp"
-    no_tp = "no_tp"
 
 
 class CrossEntropyImpl(str, enum.Enum):
