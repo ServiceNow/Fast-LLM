2794fd1605087082b0ff54ec609930a02576136f
Joel Lamy-Poirier <joel.lamy-poirier@servicenow.com>
2026-01-22 17:23:44 -0500

    fix

--- fast_llm/data/preparator/dataset_discovery/__init__.py
+++
@@ -1,4 +0,0 @@
-from fast_llm.data.preparator.dataset_discovery.config import DatasetDiscoveryConfig
-from fast_llm.data.preparator.dataset_discovery.prepare import DatasetDiscoveryPreparator
-
-__all__ = ["DatasetDiscoveryConfig", "DatasetDiscoveryPreparator"]
